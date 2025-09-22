2be2798d874e5f09af7757c745858185bf0592b2
Realgi022 <realgihila@gmail.com>
2025-09-22 16:55:00 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FootballBettingWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
